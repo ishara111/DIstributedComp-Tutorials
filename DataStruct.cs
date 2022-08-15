d62752ef0e3658c11798cb12ed5164648c91ce26
ishara111 <isharagomes111@gmail.com>
2022-08-15 19:17:58 +0530

    added 2 classes

--- DataStruct.cs
+++ DataStruct.cs
@@ -8,5 +8,18 @@ namespace Tutorial1
 {
     internal class DataStruct
     {
+        public uint acctNo;
+        public uint pin;
+        public int balance;
+        public string firstName;
+        public string lastName;
+        public DataStruct()
+        {
+            acctNo = 0;
+            pin = 0;
+            balance = 0;
+            firstName = "";
+            lastName = "";
+        }
     }
 }
---
+++ DatabaseClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    public class DatabaseClass
+    {
+        List<DataStruct> dataStruct;
+        public DatabaseClass()
+        {
+            dataStruct = new List<Tutorial1.DataStruct>();
+        }
+        public uint GetAcctNoByIndex(int index)
+        {
+
+        }
+        public uint GetPINByIndex(int index)
+        {
+
+        }
+        public string GetFirstNameByIndex(int index)
+        {
+
+        }
+        public string GetLastNameByIndex(int index)
+        {
+
+        }
+
+    }
+}
