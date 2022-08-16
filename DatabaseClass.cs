01794062c239e5095dbc6bfd5aaad8bf35ebdf23
ishara111 <isharagomes111@gmail.com>
2022-08-16 13:40:59 +0530

    database class stuff added

--- DatabaseClass.cs
+++ DatabaseClass.cs
@@ -9,26 +9,48 @@ namespace Tutorial1
     public class DatabaseClass
     {
         List<DataStruct> dataStruct;
+        DataStruct data;
+        DatabaseGen dbGen;
         public DatabaseClass()
         {
             dataStruct = new List<Tutorial1.DataStruct>();
+            data = new DataStruct();
+            dbGen = new DatabaseGen();
+            addData();
+        }
+        private void addData()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                dbGen.GetNextAccount(out data.pin, out data.acctNo, out data.firstName, out data.lastName, out data.balance);
+                dataStruct.Add(data);
+            }
         }
         public uint GetAcctNoByIndex(int index)
         {
-
+            return dataStruct[index].acctNo;
         }
         public uint GetPINByIndex(int index)
         {
-
+            return dataStruct[index].pin;
         }
         public string GetFirstNameByIndex(int index)
         {
-
+            return dataStruct[index].firstName;
         }
         public string GetLastNameByIndex(int index)
         {
-
+            return dataStruct[index].lastName;
         }
+        public int GetBalanceByIndex(int index)
+        {
+            return dataStruct[index].balance;
+        }
+        public int GetNumRecords()
+        {
+            return (int)dataStruct.Count;
+        }
+
 
     }
 }
