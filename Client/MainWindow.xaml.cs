a8fff78a6fd61085b078d4c703fc69f1f8819cd1
ishara111 <isharagomes111@gmail.com>
2022-08-19 14:24:38 +0530

    initial commit

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RemotingServer;
+
+namespace client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ServerInterface foob;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChannelFactory<RemotingServer.ServerInterface> foobFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+            //Set the URL and create the connection!
+            string URL = "net.tcp://localhost:8100/DataService";
+            foobFactory = new ChannelFactory<ServerInterface>(tcp, URL);
+            foob = foobFactory.CreateChannel();
+            //Also, tell me how many entries are in the DB.
+            indexBox.Text = foob.GetNumEntries().ToString();
+        }
+
+        private void sButton_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 0;
+            int res = 0;
+            if (!int.TryParse(indexBox.Text, out res))
+            {
+                indexBox.Text = "incorrect";
+            }
+            else
+            {
+                index = Int32.Parse(indexBox.Text);
+
+                //Console.WriteLine(int.TryParse(index.ToString(), out res));
+                if ((index > 0) && (index <= foob.GetNumEntries()))
+                {
+                    string fName = "", lName = "", image = "";
+                    int bal = 0;
+                    uint acct = 0, pin = 0;
+                    //On click, Get the index....
+                    //index = Int32.Parse(indexBox.Text);
+                    //Then, run our RPC function, using the out mode parameters...
+                    foob.GetValuesForEntry(index, out acct, out pin, out bal, out fName, out lName, out image);
+                    //And now, set the values in the GUI!
+                    fNameBox.Text = fName;
+                    lNameBox.Text = lName;
+                    balanceBox.Text = bal.ToString("C");
+                    accNoBox.Text = acct.ToString();
+                    pinBox.Text = pin.ToString("D4");
+                    imageBox.Source = new BitmapImage(new Uri(image));
+                }
+                else
+                {
+                    indexBox.Text = "incorrect";
+                }
+            }
+        }
+    }
+}
---
+++ DatabaseLib/DatabaseClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLib
+{
+    public class DatabaseClass
+    {
+        List<DataStruct> dataStruct;
+        //DataStruct data;
+        //DatabaseGen dbGen;
+        public DatabaseClass()
+        {
+            dataStruct = new List<DatabaseLib.DataStruct>();
+
+
+            addData();
+        }
+        private void addData()
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+                DataStruct data = new DataStruct();
+                DatabaseGen dbGen = new DatabaseGen();
+                dbGen.GetNextAccount(out data.pin, out data.acctNo, out data.firstName, out data.lastName, out data.balance, out data.image);
+                dataStruct.Add(data);
+                //Console.WriteLine(data.image);
+            }
+        }
+
+        public uint GetAcctNoByIndex(int index)
+        {
+            return dataStruct[index - 1].acctNo;
+        }
+        public uint GetPINByIndex(int index)
+        {
+            return dataStruct[index - 1].pin;
+        }
+        public string GetFirstNameByIndex(int index)
+        {
+            return dataStruct[index - 1].firstName;
+        }
+        public string GetLastNameByIndex(int index)
+        {
+            return dataStruct[index - 1].lastName;
+        }
+        public int GetBalanceByIndex(int index)
+        {
+            return dataStruct[index - 1].balance;
+        }
+        public string GetProfileImage(int index)
+        {
+            return dataStruct[index - 1].image;
+        }
+        public int GetNumRecords()
+        {
+            return (int)dataStruct.Count;
+        }
+
+
+    }
+}
