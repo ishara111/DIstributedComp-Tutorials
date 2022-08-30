a4ae06d08db09b3f58719e4ed64c91515e4db879
ishara111 <isharagomes111@gmail.com>
2022-08-30 11:41:59 +0530

    added API and dataclass projects

---
+++ APIClasses/DataIntermed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClasses
+{
+    public class DataIntermed
+    {
+        public int bal;
+        public uint acct;
+        public uint pin;
+        public string fname;
+        public string lname;
+        public string image;
+    }
+}
---
+++ BusinessTierWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BusinessTierWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
