97a3af434e429257df38c2ef60463ce5da1a4235
ishara111 <isharagomes111@gmail.com>
2022-09-09 18:22:47 +0530

    token delete front end part added

--- Authenticator/Authenticate.cs
+++ Authenticator/Authenticate.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Authenticator
 {
     internal class Authenticate : AuthenticateInterface
     {
+        public int time { get; set; }
         public int Login(string name, string Password)
         {
             throw new NotImplementedException();
@@ -25,6 +27,11 @@ namespace Authenticator
 
         internal void ClearTokens()
         {
+            while(true)
+            {
+                Console.WriteLine("cleared " + time);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
--- Authenticator/Program.cs
+++ Authenticator/Program.cs
@@ -3,23 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Authenticator
 {
     internal class Program
     {
+        static Authenticate authenticate;
+        static Thread cleartokens;
         static void Main(string[] args)
         {
+            authenticate = new Authenticate();
+
             Console.WriteLine("Authentication Server");
+
+            Thread connection = new Thread(Connect);
+            Thread clearMenu = new Thread(ClearMenu);
+            cleartokens = new Thread(Clear);
+            connection.Start();
+            Thread.Sleep(1000);
+            clearMenu.Start();
+
+            authenticate.time = 180;
+
+            cleartokens.Start();
+
+        }
+
+        private static void Connect()
+        {
             ServiceHost host;
             NetTcpBinding tcp = new NetTcpBinding();
             host = new ServiceHost(typeof(Authenticate));
             host.AddServiceEndpoint(typeof(AuthenticateInterface), tcp, "net.tcp://0.0.0.0:8100/AuthenticationService");
             host.Open();
             Console.WriteLine("Server Online");
-            Console.ReadLine();
             host.Close();
+        }
+
+        private static void ClearMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Default time Interval to delete tokens: 3 mins");
+            while (true)
+            {
+                int res;
+                Console.WriteLine();
+                Console.WriteLine("enter time interval to delete tokens in seconds");
+                string time = Console.ReadLine();
+                if (int.TryParse(time,out res))
+                {
+                    authenticate.time = int.Parse(time);
+                }
+                else
+                {
+                    Console.WriteLine("Input must be integer");
+                }
+            }
+        }
+
+        private static void Clear()
+        {
+            authenticate.ClearTokens();
         }
     }
 }
