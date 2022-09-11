3d9dcfa34a8b908e702569492bc10f13f85a07bf
ishara111 <isharagomes111@gmail.com>
2022-09-11 21:55:37 +0530

    Services.txt now deletes on startup

--- Registry/App_Start/WebApiConfig.cs
+++ Registry/App_Start/WebApiConfig.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Http;
 
 namespace Registry
 {
     public static class WebApiConfig
     {
+        private static string folder = HttpContext.Current.Server.MapPath("~/App_Data");
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -20,6 +23,11 @@ namespace Registry
                 defaults: new { id = RouteParameter.Optional }
             );
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json"));
+
+            if (File.Exists(folder + "/Services.txt")) //this condition deletes the services.txt if its present
+            {
+                File.Delete(folder + "/Services.txt");
+            }
         }
     }
 }
