5768b1ad80b1c576d0d2147db5a0c05d97e98aa6
ishara111 <isharagomes111@gmail.com>
2022-10-10 18:45:29 +0530

    login added

---
+++ WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+
+        // POST: api/Login
+        public IHttpActionResult Post(string username,string password)
+        {
+            if (username.Equals("admin") && password.Equals("adminpass"))
+            {
+                return Ok("logged in");
+            }
+            else
+            {
+                return BadRequest("incorrect username or password");
+            }
+        }
+
+    }
+}
